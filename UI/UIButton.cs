2c9f1b6b549e1c59c87bb7b05ac8abc9405a17ba
Angel Castillo <scardking@gmail.com>
2020-02-29 23:11:39 -0500

    reorganizing

---
+++ UI/UIButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour
+{
+    private Button button;
+
+    private void Awake()
+    {
+        if (GetComponent<Button>())
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
+        }
+    }
+
+    protected virtual void OnClick() {
+        Debug.Log(gameObject.name);
+
+        if (gameObject.name == "Continue") References.player.ToggleMenu();
+        else if (gameObject.name == "Exit") Application.Quit();
+        else if (gameObject.name == "TargetAir") References.player.ToggleAirTarget();
+        else if (gameObject.name == "CursorMode") References.player.ToggleArchitect();
+        else if (gameObject.name == "AutoSelectWall") References.player.ToggleExtend();
+        else if (gameObject.name == "Replace") References.player.ToggleReplace();
+        else if (gameObject.name == "AutoSelectMatching") References.player.ToggleExtendMatching();
+    }
+}
