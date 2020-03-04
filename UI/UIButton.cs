60ba1eee90903337399ffe3cceb9179bcdb2a170
Angel Castillo <scardking@gmail.com>
2020-03-04 09:17:39 -0500

    hotkey method

--- UI/UIButton.cs
+++ UI/UIButton.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class UIButton : MonoBehaviour
@@ -10,20 +12,32 @@ public class UIButton : MonoBehaviour
 
     private void Awake()
     {
-        if (!GetComponent<Button>()) return;
-        player = References.player;
-        button = GetComponent<Button>();
-        button.onClick.AddListener(OnClick);
+        if (GetComponent<Button>())
+        {
+            player = References.player;
+            button = GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
+        }
+        Transform control = transform.Find("Control");
+        if (control)
+        {
+            control.GetComponent<TMP_InputField>().onValueChanged.AddListener(ChangeHotkey);
+        }
+    }
+
+    private void ChangeHotkey(string input)
+    {
+        Debug.Log(input);
     }
 
     private void OnClick() {
         if (gameObject.name == "Continue") player.ToggleMenu("Main Menu");
         else if (gameObject.name == "Abilities") player.ToggleMenu("Abilities Menu");
         else if (gameObject.name == "Exit") Application.Quit();
-        else if (gameObject.name == "TargetAir") player.ToggleAirTarget();
-        else if (gameObject.name == "CursorMode") player.ToggleArchitect();
-        else if (gameObject.name == "AutoSelectWall") player.ToggleExtend();
+        else if (gameObject.name == "Target Air") player.ToggleAirTarget();
+        else if (gameObject.name == "Cursor Mode") player.ToggleArchitect();
+        else if (gameObject.name == "Auto Select Wall") player.ToggleExtend();
         else if (gameObject.name == "Replace") player.ToggleReplace();
-        else if (gameObject.name == "AutoSelectMatching") player.ToggleExtendMatching();
+        else if (gameObject.name == "Auto Select Matching") player.ToggleExtendMatching();
     }
 }
